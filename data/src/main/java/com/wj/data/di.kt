4f971a50f60279fa0900776f958b488e3f107c3c
wjankowski-oke <78346915+wjankowski-oke@users.noreply.github.com>
2021-09-02 23:25:05 +0200

    Created BreweriesApi, Brewery model, BreweryPojo model, BreweryRepo, BreweryRepoImpl, added koin to data and domain modules

---
+++ data/src/main/java/com/wj/data/di.kt
@@ -0,0 +1,35 @@
+package com.wj.data
+
+import com.wj.data.api.BreweriesApi
+import com.wj.data.repo.BreweryRepoImpl
+import com.wj.domain.repo.BreweryRepo
+import okhttp3.OkHttpClient
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val dataModule = module {
+    val BASE_URL = ""
+
+    single<BreweryRepo> { BreweryRepoImpl(get()) }
+
+    single<OkHttpClient> {
+        OkHttpClient.Builder()
+            .build()
+    }
+
+    single<Retrofit> {
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .client(get())
+            .build()
+    }
+
+    single<BreweriesApi> {
+        get<Retrofit>().create(
+            BreweriesApi::class.java
+        )
+    }
+
+}
