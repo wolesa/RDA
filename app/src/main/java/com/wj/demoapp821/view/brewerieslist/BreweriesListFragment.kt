c1036286c514edbd49bfef8acc81003956b475b3
wjankowski-oke <78346915+wjankowski-oke@users.noreply.github.com>
2021-09-03 00:57:59 +0200

    Added activity view model and refactored BreweriesListFragment.kt

--- app/src/main/java/com/wj/demoapp821/view/brewerieslist/BreweriesListFragment.kt
+++ app/src/main/java/com/wj/demoapp821/view/brewerieslist/BreweriesListFragment.kt
@@ -6,27 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.wj.demoapp821.R
+import com.wj.demoapp821.view.main.MainViewModel
 
-class BreweriesListFragment : Fragment() {
+class BreweriesListFragment : Fragment(R.layout.breweries_list_fragment) {
 
-    companion object {
-        fun newInstance() = BreweriesListFragment()
-    }
+    private val viewModel: MainViewModel by activityViewModels()
 
-    private lateinit var viewModel: BreweriesListViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.breweries_list_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(BreweriesListViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
