5184218be015924a075b8c6141ce40b3196289ce
wjankowski-oke <78346915+wjankowski-oke@users.noreply.github.com>
2021-09-03 11:08:51 +0200

    Implemented observing for breweries list and displaying it

--- app/src/main/java/com/wj/demoapp821/view/brewerieslist/BreweriesListFragment.kt
+++ app/src/main/java/com/wj/demoapp821/view/brewerieslist/BreweriesListFragment.kt
@@ -1,18 +1,29 @@
 package com.wj.demoapp821.view.brewerieslist
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.wj.demoapp821.R
+import com.wj.demoapp821.utils.findView
+import com.wj.demoapp821.utils.observeStateFlow
 import com.wj.demoapp821.view.main.MainViewModel
 
 class BreweriesListFragment : Fragment(R.layout.breweries_list_fragment) {
 
     private val viewModel: MainViewModel by activityViewModels()
+    private val breweriesList: RecyclerView? by findView(R.id.breweriesList)
+    private val adapter = BreweriesAdapter()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        breweriesList?.adapter = adapter
+
+        observeStateFlow(viewModel.breweriesListStateFlow) {
+            adapter.submitList(it)
+        }
+
+    }
 }
