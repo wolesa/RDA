ef7a40b173eb94bfa3dd23d9002a24329513e2ba
wjankowski-oke <78346915+wjankowski-oke@users.noreply.github.com>
2021-09-06 19:55:10 +0200

    Optimised imports in MainActivity

--- app/src/main/java/com/wj/demoapp821/view/main/MainActivity.kt
+++ app/src/main/java/com/wj/demoapp821/view/main/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.wj.demoapp821.view.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.wj.demoapp821.R
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel by viewModel<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
