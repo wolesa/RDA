acf3220c082eb68fa4e9d58233e8c7acc04d84fa
wjankowski-oke <78346915+wjankowski-oke@users.noreply.github.com>
2021-09-06 19:54:29 +0200

    Removed not used methods and fields in MainViewModel, formatted code

--- app/src/main/java/com/wj/demoapp821/view/main/MainViewModel.kt
+++ app/src/main/java/com/wj/demoapp821/view/main/MainViewModel.kt
@@ -6,7 +6,6 @@ import com.wj.domain.interactor.GetBreweriesUseCase
 import com.wj.domain.model.Brewery
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class MainViewModel(private val getBreweriesUseCase: GetBreweriesUseCase) : ViewModel() {
@@ -17,42 +16,21 @@ class MainViewModel(private val getBreweriesUseCase: GetBreweriesUseCase) : View
     private val _breweryDetailsStateFlow = MutableStateFlow<Brewery?>(null)
     val breweryDetailsStateFlow = _breweryDetailsStateFlow.asStateFlow()
 
-    private var city: String? = null
-    private var name: String? = null
     private var pageNumber: Int = 0
 
     init {
-
-        breweriesListStateFlow.collect {  }
-        searchBreweries()
-    }
-
-    fun searchBreweries(city: String? = null,
-                        name: String? = null, ){
-        val isNewSearch = this.city != city || this.name != name
-
-        if(isNewSearch){
-            pageNumber = 0
-            this.city = city
-            this.name = name
-        }
-
         getNextPage()
     }
 
-    private fun getNextPage() {
+    fun getNextPage() {
         pageNumber++
 
         viewModelScope.launch {
-            _breweriesListStateFlow.apply {
-                value = value + getBreweriesUseCase.getBreweries(
-                    city,
-                    name,
-                    pageNumber
-                )
-            }
+            val breweries = getBreweriesUseCase.getBreweries(
+                pageNumber = pageNumber
+            )
+
+            _breweriesListStateFlow.value = breweries
         }
     }
-
-
 }
